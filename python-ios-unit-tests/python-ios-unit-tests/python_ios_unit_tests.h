ab374e7dc38422f39c59ff2080bd549bca82f7e2
Nicolas Lelong <rotoglup@gmail.com>
2012-04-06 11:29:56 +0200

    first ultra simple unit test that builds and run

--- python-ios-unit-tests/python-ios-unit-tests/python_ios_unit_tests.h
+++ python-ios-unit-tests/python-ios-unit-tests/python_ios_unit_tests.h
@@ -1,11 +1,3 @@
-//
-//  python_ios_unit_tests.h
-//  python-ios-unit-tests
-//
-//  Created by mgdesign SARL on 05/04/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <SenTestingKit/SenTestingKit.h>
 
 @interface python_ios_unit_tests : SenTestCase
